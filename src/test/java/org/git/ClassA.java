a87d9cace340706b8036f3d1162aedc53ff7cdcc
Senthilraja760 <senthilraja760@gmail.com>
2022-09-18 23:29:05 +0530

    2 methods

--- src/test/java/org/git/ClassA.java
+++ src/test/java/org/git/ClassA.java
@@ -1,5 +1,13 @@
 package org.git;
 
 public class ClassA {
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
